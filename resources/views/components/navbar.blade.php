7e24f5b1a5c6771e96450f29cbe981f81a196b53
Steve Beaudry <live.remix@gmail.com>
2023-08-24 19:45:46 -0400

    Initial home page

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav>
+    <img src="{{ asset('images/logo.png') }}" alt="Logo">
+    <ul>
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('home') }}">What is EV</a></li>
+        <li><a href="{{ route('home') }}">Products</a></li>
+        <li><a href="{{ route('home') }}">Incentives</a></li>
+        <li><a href="{{ route('home') }}">Contact Us</a></li>
+        <li><a href="{{ route('home') }}">About Us</a></li>
+        <li><a href="{{ route('home') }}">News</a></li>
+        <li><a href="{{ route('home') }}">ARS Services</a></li>
+    </ul>
+</nav>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <div class="hero">
+        <h1>EV Charging Station Integration Specialists</h1>
+    </div>
+</x-layout>
